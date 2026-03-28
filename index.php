47ba90d84f40e3f556dd4b6d7025ad9cff622790
Natkamon Jareansuk <natkamonj66@nu.ac.th>
2026-03-28 19:12:22 +0700

    up

--- index.php
+++ index.php
@@ -1,4 +1,19 @@
 <?php
-header("Location: customer/");
-exit();
 ?>
+<!DOCTYPE html>
+<html lang="th">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Sports Equipment Management System</title>
+</head>
+<body>
+    <h1>ระบบจัดการคลังอุปกรณ์กีฬา</h1>
+    <ul>
+        <li><a href="customer/">customer</a></li>
+        <li><a href="executive/">executive</a></li>
+        <li><a href="staff/">staff</a></li>
+        <li><a href="warehouse/">warehouse</a></li>
+    </ul>
+</body>
+</html>
